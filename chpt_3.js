695354f54ae4fc86131a53e0a0e6f8216f100d1e
Elukoye <lukoyedith@gmail.com>
2020-12-29 10:57:01 +0300

    head first Js chpt 3 exercises

---
+++ chpt_3.js
@@ -0,0 +1,27 @@
+function bark(name,weight){
+    if(weight > 20){
+        console.log(name + " says WOOF WOOF");
+    }else{
+        console.log(name + " says woof woof");
+    }
+}
+
+bark("rover", 23);
+bark("spot", 13);
+bark("spike", 53);
+bark("lady", 17);
+bark("fido", "20");
+bark("scottie", -1);
+
+
+function doIt(param){
+    param = 2;
+}
+var test = 1 ;
+doIt(test);
+console.log(test)
+
+function testParam(name,num){
+    console.log(name,num)
+}
+testParam(7,"mei")
